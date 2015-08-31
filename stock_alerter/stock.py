16d6432b8631f10edc255873165d0d2ad9131867
Daniel Frank <daniel.frank@lin-digital.com>
2015-08-31 14:05:58 -0700

    Reorganize tests

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,16 +1,5 @@
-import unittest
-
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
         self.price = None
-
-class StockTest(unittest.TestCase):
-    def test_price_of_a_new_stock_class_should_be_none(self):
-        stock = Stock("GOOG")
-        self.assertIsNone(stock.price)
-
-if __name__ == "__main__":
-    unittest.main()
-
 
