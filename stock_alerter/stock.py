9c259305b50d69662c82b76ef09482daaa892526
Daniel Frank <daniel.frank@lin-digital.com>
2015-09-02 13:43:23 -0700

    Further cleanup

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -35,29 +35,24 @@ class Stock:
             self.history[-2].value < self.history[-1].value
 
     def get_crossover_signal(self, on_date):
-        prev_date = on_date - timedelta(1)
 
         long_term_ma = MovingAverage(self.history, self.LONG_TERM_TIMESPAN)
         short_term_ma = MovingAverage(self.history, self.SHORT_TERM_TIMESPAN)
 
         try:
-            long_term_ma_value = long_term_ma.value_on(on_date)        
-            prev_long_term_ma_value = long_term_ma.value_on(prev_date)       
-            short_term_ma_value = short_term_ma.value_on(on_date)        
-            prev_short_term_ma_value = short_term_ma.value_on(prev_date)
+            if self._is_crossover_below_to_above(on_date, short_term_ma, long_term_ma):
+                return StockSignal.buy
+            if self._is_crossover_below_to_above(on_date, long_term_ma, short_term_ma):
+                return StockSignal.sell
         except NotEnoughDataException:
             return StockSignal.neutral
         
-        if self._is_crossover_below_to_above(prev_short_term_ma_value, prev_long_term_ma_value, short_term_ma_value, long_term_ma_value):
-            return StockSignal.buy
-
-        if self._is_crossover_below_to_above(prev_long_term_ma_value, prev_short_term_ma_value, long_term_ma_value,  short_term_ma_value):
-            return StockSignal.sell
 
         return StockSignal.neutral
 
 
-    def _is_crossover_below_to_above(self, prev_ma, prev_reference_ma,
-                                    current_ma, current_reference_ma):
-        return prev_ma < prev_reference_ma \
-               and current_ma > current_reference_ma
+    def _is_crossover_below_to_above(self, on_date, ma, reference_ma):
+        prev_date = on_date - timedelta(1)
+
+        return ma.value_on(prev_date) < reference_ma.value_on(prev_date)   \
+               and ma.value_on(on_date) > reference_ma.value_on(on_date)
