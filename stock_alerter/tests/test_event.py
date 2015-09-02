9d36aeeab222e1c36d3604b803925d97819a54c4
Daniel Frank <daniel.frank@lin-digital.com>
2015-09-02 14:50:31 -0700

    Add Event object and tests w/dummy function

---
+++ stock_alerter/tests/test_event.py
@@ -0,0 +1,27 @@
+import unittest
+import collections
+
+from ..event import Event
+
+
+class EventTest(unittest.TestCase):
+    def test_a_listener_is_notified_when_an_event_is_raised(self):
+        called = False
+        def listener():
+            nonlocal called
+            called = True
+        event = Event()
+        event.connect(listener)
+        event.fire()
+        self.assertTrue(called)
+
+    def test_a_listener_is_passed_right_parameters(self):
+        params = ()
+        def listener(*args, **kwargs):
+            nonlocal params
+            params = (args, kwargs)
+        event = Event()
+        event.connect(listener)
+        event.fire(5, shape="square")
+        self.assertEquals(((5, ), {"shape":"square"}),params)
+
