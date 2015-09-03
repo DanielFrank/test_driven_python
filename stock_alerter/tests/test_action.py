fe74640b5eeaa80107e4c33beb5bbf0feca2982e
Daniel Frank <daniel.frank@lin-digital.com>
2015-09-03 16:49:37 -0700

    Use mock.patch, manually start/stop patch

--- stock_alerter/tests/test_action.py
+++ stock_alerter/tests/test_action.py
@@ -5,15 +5,14 @@ from ..action import PrintAction
 
 class PrintActionTest(unittest.TestCase):
     def test_executing_action_prints_message(self):
-        mock_print = mock.Mock()
-        old_print = __builtins__["print"]
-        __builtins__["print"] = mock_print
+        patcher = mock.patch('builtins.print')
+        mock_print = patcher.start()
         try:
             action = PrintAction()
             text = "GOOG > $10"
             action.execute(text)
             mock_print.assert_called_with(text)
         finally:
-            __builtins__["print"] = old_print
+            patcher.stop()
 
             
