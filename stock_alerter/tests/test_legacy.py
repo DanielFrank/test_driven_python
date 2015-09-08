27e2b72815cf9b23dd22ca881a8c18dcf617ac26
Daniel Frank <daniel.frank@lin-digital.com>
2015-09-08 11:05:01 -0700

    Add characterization test

---
+++ stock_alerter/tests/test_legacy.py
@@ -0,0 +1,12 @@
+import smtplib
+import unittest
+from unittest import mock
+
+from ..legacy import AlertProcessor
+
+class AlertProcessorTest(unittest.TestCase):
+    @mock.patch("builtins.print")
+    def test_processor_characterization(self, mock_print):
+        AlertProcessor()
+        mock_print.assert_has_calls([mock.call("AAPL", 8), mock.call("GOOG", 15), mock.call("AAPL", 10), mock.call("GOOG", 21)])
+
